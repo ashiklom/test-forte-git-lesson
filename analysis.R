05dddeccad50c04c8856397163c0285ad7b6ce83
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-02-22 17:46:14 -0500

    Explore the data

--- analysis.R
+++ analysis.R
@@ -2,6 +2,12 @@ library(tidyverse)
 a_bandlist_raw <- read_csv("data/A_bandlist.csv")
 a_bandlist_raw
 glimpse(a_bandlist_raw)
+
+summarize_all(a_bandlist_raw, n_distinct)
+
+count(a_bandlist_raw, Species)
+count(a_bandlist_raw, Health_status)
+count(a_bandlist_raw, Canopy_status)
 ggplot(a_bandlist) +
   aes(x = SubplotID, y = DBH_cm, fill = Species) +
   geom_boxplot()
